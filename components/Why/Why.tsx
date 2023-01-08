4a69d8c689944e179951bbd3633f52d1ed1ac600
Eyob94 <eyobsh123@gmail.com>
2023-01-08 21:29:07 +0300

    feat: Initiated the why section

---
+++ components/Why/Why.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Why = () => {
+	return (
+		<div className="flex justify-center w-full mt-10">
+			<div className="w-full max-w-5xl 2xl:max-w-7xl">
+				<div className="w-full text-3xl">
+					<span className="font-bold">Why</span> Milk Recording
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Why;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from "next";
 
 import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
+import Why from "../components/Why/Why";
 
 const Home: NextPage = () => {
 	return (
 		<>
 			<Navbar />
 			<Hero />
+			<Why />
 		</>
 	);
 };
