61a4ac3b0d7bba4322dd96343e163244cdca9eb1
Eyob94 <eyobsh123@gmail.com>
2023-01-15 21:15:31 +0300

    fix: slightly increased image quality

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -13,6 +13,7 @@ const Hero = () => {
 					src={"/front.png"}
 					width={1500}
 					height={500}
+					quality={100}
 					alt={"dairy data image"}
 					objectFit={"cover"}
 					layout={"fill"}
@@ -23,6 +24,7 @@ const Hero = () => {
 					src={"/mobile_front.png"}
 					width={500}
 					height={500}
+					quality={100}
 					alt={"dairy data image"}
 					objectFit={"cover"}
 					layout={"fill"}
